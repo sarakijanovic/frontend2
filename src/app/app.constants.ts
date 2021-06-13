a8f2812b665d72fbaeddebb545fdf1eca5798492
sarakijanovic <sara.kijanovic@unc.ac.rs>
2021-06-13 11:42:45 +0200

    izmena

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -9,5 +9,5 @@ export const SMER_URL = 'https://rva-bekend-gr2.herokuapp.com/smer';
 export const PROJEKAT_URL = 'https://rva-bekend-gr2.herokuapp.com/projekat';
 export const GRUPA_URL = 'https://rva-bekend-gr2.herokuapp.com/grupa';
 export const STUDENT_URL = 'https://rva-bekend-gr2.herokuapp.com/student';
-export const STUDENTI_GRUPE = 'https://rva-bekend-gr2.herokuapp.comstudentiGrupa';
+export const STUDENTI_GRUPE = 'https://rva-bekend-gr2.herokuapp.com/studentiGrupa';
 
